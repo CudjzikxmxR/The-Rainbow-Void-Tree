273fe58e035e560e0071ca8cbe410c7304e1b833
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-04-24 13:23:16 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -46,7 +46,7 @@ addLayer("p", {
             description: "0.1x Rainbows\nRainbow gain now increases over time.",
             cost: new Decimal(50),
             effect() {
-                return player.time/10
+                return Date.now()/10
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
         },
