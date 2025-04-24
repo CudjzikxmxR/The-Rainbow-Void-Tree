defe7f339fc01ca405f162b34a6fb9bf1e5be64d
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-04-24 14:53:57 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -476,7 +476,7 @@ var interval = setInterval(function() {
 	}
 	tmp.scrolled = document.getElementById('treeTab') && document.getElementById('treeTab').scrollTop > 30
 	if (hasUpgrade('p', 16) && Math.random()>= 0.96) {
-		player.clickingMult+=0.001
+		makeShinies()
 	}	
 
 	updateTemp();
