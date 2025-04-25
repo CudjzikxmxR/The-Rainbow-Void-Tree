4230d5a18adef1161c2c27996c30160b265b36c1
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-04-24 23:50:27 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -55,7 +55,7 @@ addLayer("p", {
             description: "Rainbows scale based on your Amoebas.",
             cost: new Decimal(10),
             effect() {
-                return player[this.layer].points.add(1).pow(0.5)
+                return player[this.layer].points.add(1).pow(0.505)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
         },
