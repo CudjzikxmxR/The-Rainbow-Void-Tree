c6f826e36886c3a0b42948550275bbb741d9a31e
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-04-24 23:22:32 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -420,7 +420,7 @@ const cudGrade16 = {
 			if (hasUpgrade('g', 14)) {
 				player.cherryUpgrade14+=0.01
 			}
-			if (hasUpgrade('g', 15) && (Math.floor(Math.random()*10+1)==10 && hasUpgrade('g', 19))) {
+			if (hasUpgrade('g', 15) && (Math.floor(Math.random()*10+1)==10 || hasUpgrade('g', 19))) {
 				this.color = "#770000"
 				addPoints(layers['p'], layers['p'].resetGain)
 				updateMilestones(layers['p'])
