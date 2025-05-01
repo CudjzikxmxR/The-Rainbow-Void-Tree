44f2196826d1dd13cf5bafb81339e769cbcd1626
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-01 14:20:03 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -423,9 +423,9 @@ const cudGrade16 = {
 			}
 			if (hasUpgrade('g', 15) && (Math.floor(Math.random()*10+1)==10 || hasUpgrade('g', 19))) {
 				this.color = "#770000"
-				addPoints(layers["cuddy"], new Decimal(1e90))
-				updateMilestones(layers["cuddy"])
-				updateAchievements(layers["cuddy"])
+				addPoints(layers["cuddy"].layer, new Decimal(1e10))
+				updateMilestones(layers["cuddy"].layer)
+				updateAchievements(layers["cuddy"].layer)
 			} else {
 				this.color = "#6225D1"
 			}
