74e7357d1880d98bb92829ac727c89050b618a2c
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-01 19:22:30 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -148,14 +148,56 @@ addLayer("p", {
             },
         },
     },
+})
+
+addLayer("a", {
+    startData() { return {
+        unlocked: true,
+        points: new Decimal(0),
+    }},
+    color: "yellow",
+    resource: "achievement power", 
+    row: "side",
+    tooltip() { // Optional, tooltip displays when the layer is locked
+        return ("Achievements")
+    },
+    achievementPopups: true,
     achievements: {
         11: {
-            name: "IDK",
-            done() {
-                return hasUpgrade('p', 16)
-            },
+            name: "The Gimmicky Nonsense Begins",
+            image: "discord.png",
+            done() {return hasUpgrade('p', 16)},
+            goalTooltip: "Buy the <b>Activity Check</b> upgrade.<br>Award: 2x Rainbows", // Shows when achievement is not completed
+            doneTooltip: "You did it!", // Showed when the achievement is completed
         },
-    }
+    },
+    midsection: ["grid", "blank"],
+    grid: {
+        maxRows: 3,
+        rows: 2,
+        cols: 2,
+        getStartData(id) {
+            return id
+        },
+        getUnlocked(id) { // Default
+            return true
+        },
+        getCanClick(data, id) {
+            return player.points.eq(10)
+        },
+        getStyle(data, id) {
+            return {'background-color': '#'+ (data*1234%999999)}
+        },
+        onClick(data, id) { // Don't forget onHold
+            player[this.layer].grid[id]++
+        },
+        getTitle(data, id) {
+            return "Gridable #" + id
+        },
+        getDisplay(data, id) {
+            return data
+        },
+    },
 })
 
 addLayer("g", {
