3813afdb7a51d587b7148c1f1c64bf94554a05a7
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-01 15:47:12 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -416,7 +416,7 @@ const cudGrade16 = {
     },
 	onClick() {
 		if (this.color == "#006BF7") {
-			player.clickingMult+=1
+			player.clickingMult+=getClickPower()
 			player.minimumClickMult+=1
 			if (hasUpgrade('g', 14)) {
 				player.cherryUpgrade14+=0.01
