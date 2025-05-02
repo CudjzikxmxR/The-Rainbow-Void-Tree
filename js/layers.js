8cdf935d9449b554dd9c8d06299ad79409bce4fa
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-01 23:13:12 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -134,7 +134,7 @@ addLayer("p", {
             cost: new Decimal(10000000),
             style: {'width':'160px'},
             effect() {
-                return player[this.layer].points.add(1).pow(0.225*Math.min((1+Math.pow(player.clickingMult,0.5)/100), 2)).min(new Decimal(1e7))
+                return player[this.layer].points.add(1).pow(0.225*Math.min((1+Math.pow(player.clickingMult,0.5)/100), 2)).min(new Decimal(1e6))
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
             unlocked() {
