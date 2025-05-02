153c45334a9594153c3c4914d4c71fb03f376a9f
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-02 10:18:56 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -423,7 +423,7 @@ const cudGrade16 = {
 			}
 			if (hasUpgrade('g', 15) && (Math.floor(Math.random()*10+1)==10 || hasUpgrade('g', 19))) {
 				this.color = "#770000"
-				addPoints("p", getResetGain("p"))
+				//addPoints("p", getResetGain("p"))
 				updateMilestones("p")
 				updateAchievements("p")
 			} else {
