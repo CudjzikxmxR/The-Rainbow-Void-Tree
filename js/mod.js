16dd029426f16c70aaba662e97f4b8293564a146
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-02 02:49:44 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -41,7 +41,7 @@ function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(100000)
+	let gain = new Decimal(100)
 	//Cud Layer Upgrades
 	if (hasUpgrade('p', 11))
 		gain = gain.times(2)
