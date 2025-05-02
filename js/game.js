63d4a9813d7cd78f2b13fa81dbdd7af6774487ce
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-02 01:17:35 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -507,7 +507,7 @@ var interval = setInterval(function() {
 	} else {
 		resetClickMult()
 	}
-	if ((hasMilestone('g', 17)) && Math.random()>= 0.8) {
+	if ((hasMilestone('g', 17) && player['p'].feedingAxeCat) && Math.random()>= 0.8) {
 		makeShinies(catFood, 1)
 		if (player.AxeCatMult > 1) {
 			player.AxeCatMult -= 0.05
