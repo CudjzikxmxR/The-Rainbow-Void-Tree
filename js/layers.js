6fa83973c2a69f8fbd90c51541b5c32ff1444818
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-01 20:15:54 -0400

    idk33

--- js/layers.js
+++ js/layers.js
@@ -2,6 +2,7 @@ addLayer("p", {
     name: "cuddy", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    image: "resources/Amoeba_Icon.png",
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
