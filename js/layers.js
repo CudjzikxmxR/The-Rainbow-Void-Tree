4747ee2a660ff58af61415ff8962cf800ccee312
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-04 00:12:27 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -198,7 +198,7 @@ addLayer("p", {
             cost: new Decimal(10000000),
             style: {'width':'160px'},
             effect() {
-                return 1
+                return player[this.layer].clickingMult.min(100000)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
             unlocked() {
