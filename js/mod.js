fc001388bb40475ed8e73f67e2ab86a394e7dab3
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-04 12:05:45 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -163,7 +163,7 @@ function prepareTipRand() {
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("e1e1e9"))
 }
 
 // Less important things beyond this point!
