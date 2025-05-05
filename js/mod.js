1d0215d849653ff6ce3c3688cfb8ce875d4b7620
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-05 09:37:53 -0400

    idk63

--- js/mod.js
+++ js/mod.js
@@ -203,6 +203,8 @@ function getClickPower() {
 		} else {
 			baseClickPower = baseClickPower.times(Math.pow(2, player['k'].milestones.length-9))
 		}
+	if (hasMilestone('k', 21))
+		baseClickPower = baseClickPower.times(player.points.add(1).max(0).log(10))
 	if (hasUpgrade('k', 16))
 		baseClickPower = baseClickPower.times(3)
 		
