fb1296d25ea1b57cd5d30df2fce249c724735e4b
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-05 09:05:19 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -799,7 +799,7 @@ addLayer("k", {
         return false
     },
     canReset() {
-        return hasUpgrade('p', 21) && player.points.gte(this.requires())
+        return hasUpgrade('p', 21) && player.points.gte(tmp[this.layer].requires())
         //return tmp[this.layer].baseAmount.gte(tmp[this.layer].nextAt)
     },
     canBuyMax() {
