fb047f3d02e53a1be39da421cbc7af8c40c2b9a3
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-05 10:18:22 -0400

    frthistime

--- js/layers.js
+++ js/layers.js
@@ -1016,7 +1016,7 @@ addLayer("k", {
         24: {
             requirementDescription: "500 Killstreak",
             effectDescription() {
-                return "Click-related multipliers can no longer drain.<br>Click power scales based on your current click-related Rainbow multiplier.<br>Currently: "+format(upgradeEffect('p', 16).pow(0.325))+"x"
+                return "Click-related multipliers can no longer drain.<br>Click power scales based on your current click-related Rainbow multiplier.<br>Currently: "+format(upgradeEffect('p', 16).pow(0.4))+"x"
             },
             done() {return player[this.layer].best.gte(500)},
             unlocked() {return hasMilestone(this.layer, this.id-1)}
