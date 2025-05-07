1c6e0efc6e9536bc3e9c224a03b3d34d58889774
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-06 22:14:59 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -145,10 +145,12 @@ let tipMessages = [
 	"Balala > Balatro",
 	"Whenever there's a choice between multiple layers, you will be forced to play through all of the layers individually without access to the content of the other layers, excluding certain QoL features.",
 	"If it isn't clear, "+'"click power" refers to how much multiplier you gain from clicking symbols.',
+	"Huh? What's wrong with the word Festival???",
+	"R.I.P. TFS tree.",
 
 	//Update
 	"This game currently has 3 total main layers.",
-	"There are currently 26 tips in the game!",
+	"There are currently 28 tips in the game!",
 	"At this current moment of you playing this game, Stability Test 1.7 is not released.",
 ]
 let tipTick = 0
