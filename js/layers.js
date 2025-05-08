76ca88fc840a073ac091367ffaa905da049bb2e1
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-08 15:05:10 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -896,7 +896,6 @@ addLayer("g", {
         }],
         "milestones",
         //"upgrades",
-
         ["display-text", "<h3>[SET 1]</h3>"],
         ["row", [["upgrade",11],["upgrade",12],["upgrade",13]]],
         ["row", [["upgrade",14],["upgrade",15],["upgrade",16]]],
