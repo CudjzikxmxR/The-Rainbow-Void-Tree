76cf2d9731cf7421404498c0e0eb4e51d6f68b25
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-15 15:22:33 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1566,7 +1566,15 @@ addLayer("farm", {
 
         //CROPS
 
-       
+        1001: {
+            title: "Wheat",
+            description: "<i>The first crop in the entire game. A long journey awaits...</i><br><br>Value: $"+format(getCropValue(0)[0]),
+            cost: new Decimal(0),
+            style: {'width':'140px'},
+            onPurchase() {
+                player[this.layer].Crops.Wheat = getCropValue(this.id-1001)[0]
+            },
+        },
     },
 
     milestones: {
