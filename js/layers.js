c8c709660bf8bb841fefe335f0725017eac56b32
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-15 14:58:32 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1553,7 +1553,7 @@ addLayer("farm", {
                 player[this.layer].CropsAmount.Wheat = player[this.layer].CropsAmount.Wheat.add(new Decimal(-1000))
             },
             canAfford() {
-                return player[this.layer].CropsAmount.Wheat.gte(1000)
+                return player[this.layer].points.gte(1000) && player[this.layer].CropsAmount.Wheat.gte(1000)
             },
         },
     },
