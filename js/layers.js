b37df5ade8282c2ca40073aa528f1ad1f3fbac56
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-15 13:54:59 -0400

    idk102

--- js/layers.js
+++ js/layers.js
@@ -1373,6 +1373,7 @@ addLayer("farm", {
     symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     prefix: "$",
+    currencyOff: true,
     image: "resources/Knives_Icon.png",
     startData() { return {
         unlocked: false,
