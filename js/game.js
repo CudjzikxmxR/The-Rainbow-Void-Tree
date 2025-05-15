7a876b3c2e81edb6c127fc86d5980b762e29e939
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-15 14:50:14 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -563,7 +563,7 @@ var interval = setInterval(function() {
 			player.AxeCatMult = 1
 		}
 	}
-	player['farm'].points = new Decimal(1)
+	//player['farm'].points = new Decimal(1)
 
 	prepareTipRand()
 	updateTemp();
