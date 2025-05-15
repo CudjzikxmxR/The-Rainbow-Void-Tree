80207aabf042f4923b77ae87df11d12e20be7927
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-15 13:51:58 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1372,6 +1372,7 @@ addLayer("farm", {
     name: "afarm", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    prefix: "$",
     image: "resources/Knives_Icon.png",
     startData() { return {
         unlocked: false,
