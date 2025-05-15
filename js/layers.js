b60c3394d7e03fc6d0b5403d5d53f535c4d1c8aa
CudjzikxmxR <cudjzikxmxyt@gmail.com>
2025-05-15 15:23:21 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1568,7 +1568,7 @@ addLayer("farm", {
 
         1001: {
             title: "Wheat",
-            description: "<i>The first crop in the entire game. A long journey awaits...</i><br><br>Value: $"+format(1),
+            description: "<i>The first crop in the entire game. A long journey awaits...</i><br><br>Value: $"+format(getCropValue(this.id-1001)),
             cost: new Decimal(0),
             style: {'width':'140px'},
             onPurchase() {
